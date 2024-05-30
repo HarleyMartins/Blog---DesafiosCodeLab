2793b4bebe272f863fc1f0ed61e7ef25f5b5eebf
HarleyMartins <harleyfla05@gmail.com>
2024-05-30 16:26:36 -0300

    configured heart.js and animations

--- heart.js
+++ heart.js
@@ -1,5 +1,17 @@
-const icon = document.querySelector("#icon");
+var icon = document.querySelectorAll(".icon");
 
-icon.onclick = () => {
-  icon.classList.toggle("filled");
-};
+
+
+for (let i = 0; i < icon.length; i++) {
+  icon[i].onclick = () =>{
+    icon[i].classList.toggle("filled");
+  }
+  }
+
+
+// icon.onclick = () => {
+//   icon.classList.toggle("filled");
+// };
+
+
+console.log(icon)
